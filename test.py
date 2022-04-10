7c2110bcc2400bc5130c8b4e3a616179822bb654
Your Name <you@example.com>
2022-04-10 20:41:42 +0800

    working it

---
+++ test.py
@@ -0,0 +1,3 @@
+import MetaTrader5 as mt
+
+print("initialize() failed, error code =",mt.last_error())
--- trade.py
+++ trade.py
@@ -1,4 +1,6 @@
 from ast import If
+from pickletools import markobject
+from symtable import Symbol
 from turtle import position
 import MetaTrader5 as mt
 import pandas as pd
@@ -22,30 +24,6 @@ def Price(symbol,type):
 
     return price
 
-def tradebuy(symbol, volume, type):
-
-    if type == "BUY":
-        type_trade = mt.ORDER_TYPE_BUY
-        price = mt.symbol_info_tick(symbol).ask
-        print(price)
-    else:
-        type_trade = mt.ORDER_TYPE_SELL
-        price = mt.symbol_info_tick(symbol).bid
-    request = {
-        "action": mt.TRADE_ACTION_DEAL,
-        "symbol": symbol,
-        "volume": float(volume),
-        "type": type_trade,
-        "price": price,
-        "tp": 46600.00,
-        "deviation": 20,
-        "magic": 234000,
-        "type_time": mt.ORDER_TIME_GTC,
-        "type_filling": mt.ORDER_FILLING_IOC,
-    }
-
-    order = mt.order_send(request)
-    print(order)
 
 
 positions = mt.positions_get()
@@ -73,9 +51,77 @@ def calc_position_size(symbol, strategy):
     return lot_size
 
 
-lot_size = calc_position_size('AUDUSD', {
-    "account_currency": "USD",
-    "risk": 2,
-    "stopLoss": 20})
+# lot_size = calc_position_size('EURUSD', {
+#     "account_currency": "USD",
+#     "risk": 1,
+#     "stopLoss": 100})
 
-print(lot_size)
+# print(lot_size)
+def pip_value(symbol,type):
+    symbol_1 = symbol[0:3]
+    symbol_2 = symbol[3:6]
+
+    if symbol_2 == "USD":
+        return 10
+    else:
+        if type == "BUY":
+            price = mt.symbol_info_tick(symbol).ask
+        else:
+            price = mt.symbol_info_tick(symbol).bid
+        print(str(price).index('.'))
+        if str(price).index('.') >= 2:  # JPY pair
+            print('jpy')
+            multiplier = 0.01
+        else:
+            multiplier = 0.0001
+            print('nrom')
+        symbol_3 = "USD" + symbol_2
+        print(symbol_3)
+        try:
+            varpip = price = mt.symbol_info_tick("USD" + symbol_2).ask
+        except Exception:
+            varpip = price
+            print('sorry')
+        
+        pip = 100000 * multiplier  / varpip
+        return pip
+
+def lot_value(stop_pip,varpip):
+    account = mt.account_info()
+    balance = float(200000)
+    Value_per_pip = balance * 0.01 / stop_pip
+    lot = (Value_per_pip * (100000 / varpip)) / 100000
+    return round(lot,2)
+
+def tradebuy(symbol, type, stop_loss, stop_pip):
+
+
+    lot = lot_value(stop_pip,pip_value(symbol,type))
+
+    if type == "BUY":
+        type_trade = mt.ORDER_TYPE_BUY
+        price = mt.symbol_info_tick(symbol).ask
+        print(price)
+    else:
+        type_trade = mt.ORDER_TYPE_SELL
+        price = mt.symbol_info_tick(symbol).bid
+    request = {
+        "action": mt.TRADE_ACTION_DEAL,
+        "symbol": symbol,
+        "volume": float(lot),
+        "type": type_trade,
+        "price": price,
+        "tp": 46600.00,
+        "deviation": 20,
+        "magic": 234000,
+        "type_time": mt.ORDER_TIME_GTC,
+        "type_filling": mt.ORDER_FILLING_IOC,
+    }
+
+    order = mt.order_send(request)
+    print(order)
+
+
+print(lot_value(140,pip_value("asdjl","BUY")))
+
+# print(pip_calc('USDAUD'))
