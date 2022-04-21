78557a7dd32803115f6418991fec54419826d731
Tsegtsee <Tsegts-erdene.g@empasoft.mn>
2022-04-21 19:04:03 +0800

    okey

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import pyautogui
 
-list = ["tp", 212.43, "f23.34"]
-print(list)
-print(int(list[2]))
-print("this should work")
+list = ["USDCAD", "BUY", "212.df", "23.34"]
+try:
+    if(len(list[0]) == 6 and float(list[3]) and (len(list[1]) == 3 or len(list[1]) == 4)):
+        print("true")
+        print(float(list[1]))
+    else:
+        print("false")
+except:
+    print("not valid")
