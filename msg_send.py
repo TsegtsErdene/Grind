06c411a5a72a82c281d3ce40032f10899b57220b
Tsegtsee <Tsegts-erdene.g@empasoft.mn>
2022-05-12 14:47:02 +0800

    hhe

--- msg_send.py
+++ msg_send.py
@@ -1,4 +1,4 @@
-from http import client
+
 import discord
 import random
 
@@ -6,7 +6,4 @@ TOKEN = 'OTc0MTYxMzc3MDk2Nzc3NzI4.GZPk0P.bQBNnIJni2Ds3VngdhEyxbgrWklo95l6eT-mAA'
 
 client = discord.Client()
 
-async def on_ready():
-    print("wtf")
-
-client.run("TOKEN")
+print(client)
