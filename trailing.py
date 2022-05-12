f818ec26a2a7013a203ab1b651322ed77ad6fc9f
Tsegtsee <Tsegts-erdene.g@empasoft.mn>
2022-05-12 16:44:55 +0800

    function

--- trailing.py
+++ trailing.py
@@ -9,57 +9,40 @@ mt.initialize()
 
 
 def trail_sl(pos):
-    # print(pos.ticket)
 
     item = psql.get(pos.ticket)
+    print(item)
 
     if pos.type == 0:
         if(pos.price_current >= item[0][4] and round(pos.sl, 4) > item[0][3]):
-            request = {
-                'action': mt.TRADE_ACTION_SLTP,
-                'position': pos.ticket,
-                'tp': float(item[0][6]),
-                'sl': float(item[0][3]),
 
-            }
-            result = mt.order_send(request)
+            send_order(item[0][1], "tp1 reachaed", item[0][6], item[0][3])
 
-            match result.retcode:
-                case 10009:
-                    print("send message success")
-                case 10016:
-                    print("send invalid stop")
-                case 10025:
-                    print("send no change")
-                case unknown_command:
-                    print(unknown_command)
     else:
         if(pos.price_current <= item[0][4] and round(pos.sl, 4) > item[0][3]):
 
-            request = {
-                'action': mt.TRADE_ACTION_SLTP,
-                'position': pos.ticket,
-                'tp': float(item[0][6]),
-                'sl': float(item[0][3]),
+            send_order(item[0][1], "tp1 reachaed", item[0][6], item[0][3])
 
-            }
-            result = mt.order_send(request)
 
-            match result.retcode:
-                case 10009:
-                    print("send message success")
-                case 10016:
-                    print("send invalid stop")
-                case 10025:
-                    print("send no change")
-                case unknown_command:
-                    print(unknown_command)
-        # print(item)
-        # print(pos.price_current)
-        # print(item[0][4])
-        # print(" \space")
+def send_order(symbol, signal, tp, sl):
+    request = {
+        'action': mt.TRADE_ACTION_SLTP,
+        'position': pos.ticket,
+        'tp': float(tp),
+        'sl': float(sl),
 
-    # print(item)
+    }
+    result = mt.order_send(request)
+
+    match result.retcode:
+        case 10009:
+            print("send message success")
+        case 10016:
+            print("send invalid stop")
+        case 10025:
+            print("send no change")
+        case unknown_command:
+            print(unknown_command)
 
 
 if __name__ == '__main__':
